a85a1a248656c50f6abe373daa3b0f42e8245f34
elidaher <eeli56315@gmail.com>
2026-01-30 18:25:34 +0300

    change nav

--- components/FloatingNav.tsx
+++ components/FloatingNav.tsx
@@ -52,7 +52,7 @@ export const FloatingNav = ({
         }}
         transition={{ duration: 0.2 }}
         className={cn(
-          "fixed top-4 inset-x-0 mx-auto z-[5000] flex max-w-fit items-center justify-center space-x-4 rounded-full border border-transparent bg-white px-8 py-2 pr-2 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] dark:border-white/[0.15] dark:bg-neutral-900",
+          "fixed top-4 inset-x-0 mx-auto z-[5000] flex max-w-fit items-center justify-center space-x-4 rounded-full border border-transparent bg-white px-8 py-2 pr- shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] dark:border-white/[0.15] dark:bg-neutral-900",
           className,
         )}
       >
@@ -63,14 +63,14 @@ export const FloatingNav = ({
             className="flex items-center space-x-1 text-neutral-600 transition-colors hover:text-neutral-900 dark:text-neutral-300 dark:hover:text-white"
           >
             <span className="block sm:hidden">{navItem.icon}</span>
-            <span className="hidden text-sm sm:block">{navItem.name}</span>
+            <span className=" text-sm sm:block">{navItem.name}</span>
           </a>
         ))}
 
-        <button className="relative rounded-full border border-neutral-300 px-4 py-2 text-sm font-medium text-neutral-900 dark:border-white/[0.2] dark:text-white">
+        {/* <button className="relative rounded-full border border-neutral-300 px-4 py-2 text-sm font-medium text-neutral-900 dark:border-white/[0.2] dark:text-white">
           <span>Login</span>
           <span className="absolute inset-x-0 -bottom-px mx-auto h-px w-1/2 bg-gradient-to-r from-transparent via-blue-500 to-transparent" />
-        </button>
+        </button> */}
       </motion.div>
     </AnimatePresence>
   );
